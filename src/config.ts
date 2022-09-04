bd1c831a29e38dc17dce80a7533b58f4f8c642e3
Amy Dutton <amy@ahhacreative.com>
2022-09-04 10:26:43 -0500

    🎉 Initial Commit

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'SelfTeach.me';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const YOUTUBE = '';
+export const PINTEREST = '';
+export const INSTAGRAM = '';
+export const DRIBBBLE = '';
+export const GITHUB = '';
+export const LINKEDIN = '';
+export const CODEPEN = '';
+export const TWITTER = '';
+
+export const AHHA = 'https://ahhacreative.com'
+export const COMPRESSEDFM = 'https://www.compressed.fm'
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,112 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['Nunito', 'Helvetica', 'Arial', 'sans-serif'],
+			serif: ['ff-tisa-web-pro', 'serif'],
+			condensed: ['alternate-gothic-no-1-d', 'sans-serif'],
+			modern: ['bodoni-urw', 'serif'],
+			handwriting: ['northwell', 'sans-serif'],
+			mono: ['ui-monospace', 'monospace'],
+		},
+		extend: {
+			backgroundSize: {
+				half: '50%',
+			},
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			backgroundImage: (theme) => ({
+				believe: "url('/img/about__truly-believe.jpg')",
+				checkbox: "url('/img/checkbox.svg')",
+				callout: "url('/img/about__callout.jpg')",
+				unchecked: "url('/img/unchecked.svg')",
+				hero: "url('/img/bg__home.jpg')",
+				hrShort: "url('/img/hr--short.png')",
+				map: "url('/img/contact__google-map.jpg')",
+				horizontalPluses: "url('/img/pluses--horizontal.svg')",
+				pagination: "url('/img/pagination.jpg')",
+				underline: "url('/img/horizontal-line.svg')",
+			}),
+			borderWidth: {
+				1: '1px',
+			},
+			colors: {
+				almostWhite: '#efefef',
+				bunting: '#161743',
+				candyAppleRed: '#ee0e01',
+				serenade: '#FFE3D4',
+				baliHai: '#7F98AC',
+				brightPink: '#F40082',
+				gold: '#FFD200',
+				pistachio: '#95CF00',
+				java: '#26C9C3',
+				sapphire: '#3452A5',
+				black: '#000',
+				white: '#fff',
+				brightTurquoise: '#44FACD',
+				peach: '#ffc8ab',
+			},
+			flex: {
+				2: '2 2 0%',
+				2.5: '2.5 2.5 0%',
+				3: '3 3 0%',
+			},
+			fontSize: {
+				'300xl': [
+					'600px',
+					{
+						letterSpacing: '0',
+						lineHeight: '500px',
+					},
+				],
+			},
+			gridColumnEnd: {
+				'-1': '-1',
+				'-2': '-2',
+			},
+			inset: {
+				'-screen': '-100vh',
+				15: '3.75rem',
+				20: '5rem',
+				24: '6rem',
+				26: '6.5rem',
+				28: '7rem',
+			},
+			lineHeight: {
+				stacked: '.75',
+				handwriting: '0.25',
+				14: '3.5rem',
+				16: '4rem',
+				17: '4.25rem',
+				18: '4.5rem',
+				24: '6rem',
+			},
+			maxWidth: {
+				20: '5rem',
+				48: '12rem',
+				'2/3': '60%',
+			},
+			minHeight: {
+				9: '2.25rem',
+			},
+			padding: {
+				mobile: '2rem',
+				15: '3.75rem',
+				18: '4.5rem',
+			},
+			spacing: {
+				102: '25.5rem',
+			},
+			transitionProperty: {
+				transform: 'transform',
+			},
+			width: {
+				26: '6.5rem',
+			},
+			zIndex: {
+				'-1': -1,
+			},
+		},
+	},
+	plugins: [],
+}
